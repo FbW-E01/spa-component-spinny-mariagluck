e36b957cce8e28e502402f1e655aa1f43d1c1439
mariagluck <mariamart.berlin@gmail.com>
2021-07-19 22:54:40 +0200

    done and styled

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { useState, useEffect } from 'react';
+
 
 function App() {
-  return (
-    <div className="App">                                                                                                      <iframe width="560" height="315" title="yt" frameBorder="0" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1&mute=1&controls=0&showinfo=0" allow="autoplay" autoPlay></iframe>
-    </div>
-  );
-}
+  
+    const [comments, setComments] = useState(false);
 
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/comments?_delay=2000'+Math.random())
+            .then(response => response.json())
+            .then(result => setComments(result))
+    }, []);
+
+    return (
+           <div className="container">
+           <h2>Comments</h2>
+           {comments ? 
+               <ul>
+                   {comments.map((comment, i) => 
+                       <li className="commentCard" key={i}>
+                           <p><span>Title:</span> {comment.name}</p>
+                           <hr/>
+                           <p><span>By:</span> {comment.email}</p>
+                           <p><span>Comment:</span> {comment.body}</p>
+                       </li>
+                   )}
+               </ul>
+           : 
+           <FontAwesomeIcon icon={faSpinner} spin  className="spinner"/>
+           }
+     
+       </div>
+     );
+}
 export default App;
